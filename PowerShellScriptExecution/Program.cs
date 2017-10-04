9ad020059181748e7a13bb39afbb6d57f228ae1a
Matt Marchese <matthew.marchese1@gmail.com>
2017-10-04 11:13:30 -0400

    Added PowerShellScriptExecution sample

---
+++ PowerShellScriptExecution/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Management.Automation;
+using System.Collections.ObjectModel;
+using System.Threading;
+
+namespace PowerShellScriptExecution
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (PowerShell PowerShellInstance = PowerShell.Create())
+            {
+                // use "AddScript" to add the contents of a script file to the end of the execution pipeline.
+                // use "AddCommand" to add individual commands/cmdlets to the end of the execution pipeline.
+                PowerShellInstance.AddScript("Get-Process");
+                var Output = PowerShellInstance.Invoke();
+
+                foreach (var psobject in Output)
+                {
+                    Console.WriteLine(psobject.BaseObject.ToString());
+                }
+
+                Console.ReadLine();
+            }
+        }
+    }
+}
